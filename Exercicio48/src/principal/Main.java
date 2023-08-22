e656334fa0456b468217beffc21ff328260391f3
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2023-08-22 15:31:05 -0300

    Exercicio48

---
+++ Exercicio48/src/principal/Main.java
@@ -0,0 +1,33 @@
+// 48) Escreva um algoritmo para ler as notas da 1a. e 2a. avaliações de um aluno, calcule e imprima a 
+// média (simples) desse aluno. Só devem ser aceitos valores válidos durante a leitura (0 a 10) para cada nota. 
+
+package principal;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insira a nota da p1:");
+		double p1 = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.println("Insira a nota da p2:");
+		double p2 = sc.nextDouble();
+		sc.nextLine();
+		
+		double media = (p1 + p2) / 2;
+		
+		System.out.println("A média das provas foi " + media);
+		
+		sc.close();
+
+	}
+
+}
